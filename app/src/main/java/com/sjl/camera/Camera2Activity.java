8969f8f78aff66824bb37f1516155ecf746b214e
shenjl <1113225201@qq.com>
2017-08-29 15:19:03 +0800

    Camera2拍照实现

--- app/src/main/java/com/sjl/camera/Camera2Activity.java
+++ app/src/main/java/com/sjl/camera/Camera2Activity.java
@@ -15,7 +15,12 @@ import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
 import android.os.Bundle;
@@ -25,6 +30,7 @@ import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.util.Size;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -44,32 +50,10 @@ public class Camera2Activity extends Activity {
     private Context context;
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
-    private int currentCameraType = CameraCharacteristics.LENS_FACING_BACK;
-    private CameraDevice cameraDevice;
-    private CameraCaptureSession cameraCaptureSession;
-    private ImageReader imageReader;
+    private int currentCameraType = CameraCharacteristics.LENS_FACING_FRONT;
 
-    private HandlerThread handlerThread;
-    private Handler childHandler;
-    private Handler mainHandler;
+    private CameraManager cameraManager;
 
-    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice camera) {
-            cameraDevice = camera;
-            startPreview();
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice camera) {
-            stopPreview();
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice camera, int error) {
-            Toast.makeText(context, "摄像头开启失败", Toast.LENGTH_SHORT).show();
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,16 +62,22 @@ public class Camera2Activity extends Activity {
         initView();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        openCamera();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        stopPreview();
+    private void closeCamera() {
+        //关闭捕捉会话
+        if(cameraCaptureSession!=null){
+            cameraCaptureSession.close();
+            cameraCaptureSession = null;
+        }
+        //关闭相机
+        if(cameraDevice!=null){
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+        //关闭拍照处理器
+        if(imageReader!=null){
+            imageReader.close();
+            imageReader = null;
+        }
     }
 
     private void initView() {
@@ -97,17 +87,19 @@ public class Camera2Activity extends Activity {
         surfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-
+                Log.i(TAG,"surfaceCreated");
+                openCamera();
             }
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+                Log.i(TAG,"surfaceChanged");
             }
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
-
+                Log.i(TAG,"surfaceDestroyed");
+                closeCamera();
             }
         });
         findViewById(R.id.btnTakePhoto).setOnClickListener(new View.OnClickListener() {
@@ -122,70 +114,246 @@ public class Camera2Activity extends Activity {
                 switchCamera();
             }
         });
+        initLooper();
         initCamera();
     }
 
-    /**
-     * 初始化
-     */
-    private void initHandler() {
-        //Camera2全程异步
+    private HandlerThread handlerThread;
+    private Handler handler;
+
+    private void initLooper() {
         handlerThread = new HandlerThread("Camera2");
         handlerThread.start();
-        childHandler = new Handler(handlerThread.getLooper());
-        mainHandler = new Handler(getMainLooper());
+        handler = new Handler(handlerThread.getLooper());
+    }
+
+    private Size previewSize;
+
+    /**
+     * 初始化相机
+     */
+    private void initCamera() {
+        if(cameraManager==null) {
+            cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        }
     }
 
     /**
-     * 初始化摄像头
+     * 获取最大预览尺寸
+     *
+     * @param outputSizes
+     * @return
      */
-    private void initCamera() {
+    private Size getMaxSize(Size[] outputSizes) {
+        Size sizeMax = null;
+        if (outputSizes != null) {
+            sizeMax = outputSizes[0];
+            for (Size size : outputSizes) {
+                if (size.getWidth() * size.getHeight() > sizeMax.getWidth() * sizeMax.getHeight()) {
+                    sizeMax = size;
+                }
+            }
+        }
+        return sizeMax;
+    }
+
+    private CameraDevice cameraDevice;
+    //打开相机回调
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            Log.i(TAG,"onOpened");
+            //相机开启，打开预览
+            cameraDevice = camera;
+            startPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            Log.i(TAG,"onDisconnected");
+            //相机关闭
+            camera.close();
+            cameraDevice = null;
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            Log.i(TAG,"onError");
+            //相机报错
+            camera.close();
+            cameraDevice = null;
+            finish();
+        }
+    };
+
+    /**
+     * 打开相机
+     */
+    private void openCamera() {
         PermisstionUtil.requestPermissions(context, PermisstionUtil.CAMERA, 101, "正在请求拍照权限", new PermisstionUtil.OnPermissionResult() {
             @Override
             public void granted(int requestCode) {
-                initHandler();
-                initImageReader();
-//                openCamera();
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                try {
+                    //获取属性CameraDevice属性描述
+                    CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(String.valueOf(currentCameraType));
+                    //获取摄像头支持的配置属性
+                    StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    previewSize = getMaxSize(map.getOutputSizes(SurfaceHolder.class));
+                    initImageReader();
+                    //第一个参数指定哪个摄像头，第二个参数打开摄像头的状态回调，第三个参数是运行在哪个线程(null是当前线程)
+                    cameraManager.openCamera(String.valueOf(currentCameraType), stateCallback, handler);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
             public void denied(int requestCode) {
-                Toast.makeText(context, "拍照权限被禁止", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
 
-    /**
-     * 打开摄像头
-     */
-    private void openCamera() {
-        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            return;
+    ImageReader imageReader;
+
+    private void initImageReader() {
+        imageReader = ImageReader.newInstance(previewSize.getWidth(), previewSize.getHeight(), ImageFormat.JPEG, 1);
+        //监听ImageReader时间，有图像数据可用时回调，参数就是帧数据
+        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                Image image = reader.acquireLatestImage();
+                //将帧数据转换成字节数组
+                ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
+                byte[] data = new byte[byteBuffer.remaining()];
+                byteBuffer.get(data);
+                savePicture(data);
+                image.close();
+            }
+        }, null);
+    }
+
+    private CameraCaptureSession cameraCaptureSession;
+    private CaptureRequest.Builder previewBuilder;
+    //创建session回调
+    private CameraCaptureSession.StateCallback sessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession session) {
+            try {
+                cameraCaptureSession = session;
+                previewBuilder.set(CaptureRequest.CONTROL_AF_MODE, CameraMetadata.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                cameraCaptureSession.setRepeatingRequest(previewBuilder.build(), null, handler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
         }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+            cameraCaptureSession = session;
+            cameraCaptureSession.close();
+            cameraCaptureSession = null;
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    };
+
+    /**
+     * 开始预览
+     */
+    private void startPreview() {
         try {
-            cameraManager.openCamera(String.valueOf(currentCameraType), stateCallback, mainHandler);
+            previewBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            //设置预览数据输出界面
+            previewBuilder.addTarget(surfaceHolder.getSurface());
+            //创建相机捕获会话，
+            //第一个参数是捕获数据的输出Surface列表，第二个参数是CameraCaptureSession状态回调接口，第三个参数是在哪个线程(null是当前线程)
+            cameraDevice.createCaptureSession(Arrays.asList(surfaceHolder.getSurface(), imageReader.getSurface()), sessionStateCallback, handler);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
-    private void initImageReader() {
-        surfaceView.measure(0,0);
-        Log.i(TAG,surfaceView.getWidth()+","+surfaceView.getHeight());
-        imageReader = ImageReader.newInstance(surfaceView.getWidth(),surfaceView.getHeight(), ImageFormat.JPEG,1);
-        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
-            @Override
-            public void onImageAvailable(ImageReader reader) {
-                cameraDevice.close();
-                Image image = reader.acquireNextImage();
-                ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
-                byte[] bytes = new byte[byteBuffer.remaining()];
-                byteBuffer.get(bytes);
-                savePicture(bytes);
-                openCamera();
+    private CaptureRequest.Builder captureBuilder;
+    //拍完照回调
+    private CameraCaptureSession.CaptureCallback captureCallback = new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureProgressed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureResult partialResult) {
+//            super.onCaptureProgressed(session, request, partialResult);
+            try {
+//                captureBuilder.set(CaptureRequest.CONTROL_AF_MODE,CaptureRequest.CONTROL_AF_MODE_AUTO);
+                captureBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER,CaptureRequest.CONTROL_AF_TRIGGER_CANCEL);
+                //重新打开预览
+                session.setRepeatingRequest(previewBuilder.build(), null, handler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
             }
-        },mainHandler);
+        }
+
+        @Override
+        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+        }
+
+        @Override
+        public void onCaptureFailed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureFailure failure) {
+            super.onCaptureFailed(session, request, failure);
+            cameraCaptureSession.close();
+            cameraCaptureSession = null;
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    };
+
+    /**
+     * 拍照
+     */
+    private void takePhoto() {
+        try {
+            captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            captureBuilder.addTarget(imageReader.getSurface());
+            //自动对焦
+            captureBuilder.set(CaptureRequest.CONTROL_AF_MODE,CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+//            captureBuilder.set(CaptureRequest.JPEG_ORIENTATION,90);
+            cameraCaptureSession.stopRepeating();
+            //拍照
+            cameraCaptureSession.capture(captureBuilder.build(),captureCallback,handler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 切换摄像头
+     */
+    private void switchCamera() {
+        try {
+            for (String cameraId:cameraManager.getCameraIdList()){
+                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+                StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                Size maxSize = getMaxSize(map.getOutputSizes(SurfaceHolder.class));
+                if(currentCameraType==CameraCharacteristics.LENS_FACING_BACK&&characteristics.get(CameraCharacteristics.LENS_FACING)==CameraCharacteristics.LENS_FACING_FRONT){
+                    //前置转后置
+                    previewSize = maxSize;
+                    currentCameraType = CameraCharacteristics.LENS_FACING_FRONT;
+                    cameraDevice.close();
+                    openCamera();
+                    break;
+                }else if(currentCameraType==CameraCharacteristics.LENS_FACING_FRONT&&characteristics.get(CameraCharacteristics.LENS_FACING)==CameraCharacteristics.LENS_FACING_BACK){
+                    //后置转前置
+                    previewSize = maxSize;
+                    currentCameraType = CameraCharacteristics.LENS_FACING_BACK;
+                    cameraDevice.close();
+                    openCamera();
+                    break;
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -203,9 +371,9 @@ public class Camera2Activity extends Activity {
                     public void run() {
                         try {
                             Matrix matrix = new Matrix();
-                            matrix.setRotate(currentCameraType== Camera.CameraInfo.CAMERA_FACING_BACK?90:270);
-                            Bitmap bitmap = BitmapFactory.decodeByteArray(data,0,data.length);
-                            bitmap = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+                            matrix.setRotate(currentCameraType == Camera.CameraInfo.CAMERA_FACING_BACK ? 90 : 270);
+                            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
                             BitmapUtil.save(bitmap, FILEPATH + System.currentTimeMillis() + ".jpg");
                             runOnUiThread(new Runnable() {
                                 @Override
@@ -225,74 +393,6 @@ public class Camera2Activity extends Activity {
                 Toast.makeText(context, "读写权限被禁止", Toast.LENGTH_SHORT).show();
             }
         });
-    }
-    /**
-     * 开始预览
-     */
-    private void startPreview() {
-        try {
-            final CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            builder.addTarget(surfaceHolder.getSurface());
-            cameraDevice.createCaptureSession(Arrays.asList(surfaceHolder.getSurface(), imageReader.getSurface()), new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession session) {
-                    if (cameraDevice == null) {
-                        return;
-                    }
-                    cameraCaptureSession = session;
-                    try {
-                        //自动对焦
-                        builder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO);
-                        //显示预览
-                        CaptureRequest previewRequest = builder.build();
-                        cameraCaptureSession.setRepeatingRequest(previewRequest, null, childHandler);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                    Toast.makeText(context, "配置失败", Toast.LENGTH_SHORT).show();
-                }
-            }, childHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 结束预览
-     */
-    private void stopPreview() {
-        if (cameraDevice != null) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-    }
-
-    /**
-     * 拍照
-     */
-    private void takePhoto() {
-        if(cameraDevice==null){
-            return;
-        }
-        try {
-            CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
-            builder.addTarget(imageReader.getSurface());
-
-            cameraCaptureSession.capture(builder.build(),null,childHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 切换摄像头
-     */
-    private void switchCamera() {
-
     }
 
     @Override
